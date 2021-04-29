cc493f07387781563319bdbb882c90db19aef1aa
Brendalikukuvali <brendalikukuvali@gmail.com>
2021-04-30 01:10:43 +0300

    commit message

---
+++ src/components/login/login.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <h1>CHEKESHWA</h1>
+        <div className="header">Login</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Phone Number</label>
+              <input type="text" name="phone number" placeholder="phone number" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn"><a href>Login</a>
+            
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
